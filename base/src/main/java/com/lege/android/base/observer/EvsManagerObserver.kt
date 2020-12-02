e86cbfff48b132e568a0a2e33395b2dadc73e1f9
xgx01 <1179997396@qq.com>
2020-12-02 17:54:11 +0800

    增加调用语音的观察者模式

---
+++ base/src/main/java/com/lege/android/base/observer/EvsManagerObserver.kt
@@ -0,0 +1,44 @@
+package com.lege.android.base.observer
+
+import android.app.Activity
+import android.util.ArrayMap
+
+
+object EvsManagerObserver {
+    interface IEvsManagerObserver {
+        fun onObservaOpenEvsDialog()
+    }
+    private var observableMap: MutableList<String>? = ArrayList()
+    private var observers: ArrayMap<String, IEvsManagerObserver>? = ArrayMap()
+
+    //注册观察者
+    fun registerObserver(observerName:String,observer: IEvsManagerObserver){
+        if(observers == null){
+            observers = ArrayMap()
+        }
+        if(!observers!!.containsKey(observerName)){
+            observers!![observerName] = observer
+        }
+    }
+
+    //注册被观察者
+    fun registerObservable(activityName: String) {
+        if (observableMap == null) {
+            observableMap = ArrayList()
+        }
+        if (!observableMap!!.contains(activityName)) {
+            observableMap!!.add(activityName)
+        }
+    }
+
+    //由被观察者调用
+    fun onActivityOnCreate(activity: Activity) {
+        observableMap?.let {
+            if (it.contains(activity.javaClass.simpleName)) {
+                observers?.forEach { it.value.onObservaOpenEvsDialog() }
+            }
+        }
+    }
+
+
+}
